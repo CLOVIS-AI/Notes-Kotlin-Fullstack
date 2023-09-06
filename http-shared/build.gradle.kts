1adbf8712a621969bbd2b67f10452ba9bc292cb1
Ivan “CLOVIS” Canet <ivan.canet@4sh.fr>
2023-09-06 17:39:16 +0200

    refactor(http): Configure KotlinX.Serialization

--- http-shared/build.gradle.kts
+++ http-shared/build.gradle.kts
@@ -1,8 +1,24 @@
 plugins {
 	id("conventions.kotlin")
+	alias(libs.plugins.kotlinx.serialization)
 }
 
 kotlin {
 	jvm()
 	linuxX64()
+
+	val commonMain by sourceSets.getting {
+		dependencies {
+			api(libs.kotlinx.serialization.core)
+		}
+	}
+
+	val commonTest by sourceSets.getting {
+		dependencies {
+			implementation(libs.kotlin.test)
+
+			implementation(libs.kotlinx.serialization.json)
+			implementation(libs.kotlinx.coroutines.test)
+		}
+	}
 }
---
+++ http-shared/src/commonMain/kotlin/Ping.kt
@@ -0,0 +1,8 @@
+package opensavvy.note.http.shared
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+class Ping(
+	val id: Int,
+)
